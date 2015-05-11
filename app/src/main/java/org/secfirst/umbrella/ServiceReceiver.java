383d12138b32a29ee0b97851af4d50dd5e953540
Rok Biderman <rok.biderman@gmail.com>
2015-05-11 09:46:23 +0100

    change to logout and refresh handler

--- app/src/main/java/org/secfirst/umbrella/ServiceReceiver.java
+++ app/src/main/java/org/secfirst/umbrella/ServiceReceiver.java
@@ -3,12 +3,9 @@ package org.secfirst.umbrella;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
+import org.secfirst.umbrella.util.Global;
 import org.secfirst.umbrella.util.UmbrellaUtil;
-
-import java.text.SimpleDateFormat;
-import java.util.Locale;
 
 public class ServiceReceiver extends BroadcastReceiver
 {
@@ -19,11 +16,13 @@ public class ServiceReceiver extends BroadcastReceiver
     }
 
     public void callRefresh(Context context) {
-        Toast.makeText(context, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.UK).toString(), Toast.LENGTH_SHORT).show();
+//        Toast.makeText(context, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.UK).toString(), Toast.LENGTH_SHORT).show();
         UmbrellaUtil.getFeeds(context);
     }
 
     public void callLogout(Context context) {
+        Global global = (Global) context.getApplicationContext();
+        global.setLoggedIn(false);
         Intent i = new Intent(context, LoginActivity.class);
         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(i);
