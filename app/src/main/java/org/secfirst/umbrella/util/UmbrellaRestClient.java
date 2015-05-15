88db6677dea24289e87dc094e827e7502d143740
Rok Biderman <rok.biderman@gmail.com>
2015-05-15 15:48:16 +0100

    fir for accepting wildcard certificate

--- app/src/main/java/org/secfirst/umbrella/util/UmbrellaRestClient.java
+++ app/src/main/java/org/secfirst/umbrella/util/UmbrellaRestClient.java
@@ -7,6 +7,8 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.conn.ssl.X509HostnameVerifier;
 import org.secfirst.umbrella.R;
 
 public class UmbrellaRestClient {
@@ -14,13 +16,30 @@ public class UmbrellaRestClient {
     private static final String BASE_URL = "https://api.secfirst.org";
     private static final String VERSION = "v1";
 
-    private static AsyncHttpClient client = new AsyncHttpClient();
+    private static AsyncHttpClient client;
+
+    public UmbrellaRestClient() {
+        client = getTolerantClient();
+    }
+
+    public static AsyncHttpClient getTolerantClient() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client
+        SSLSocketFactory sslSocketFactory = (SSLSocketFactory) client.getHttpClient().getConnectionManager().getSchemeRegistry().getScheme("https")
+                .getSocketFactory();
+        final X509HostnameVerifier delegate = sslSocketFactory.getHostnameVerifier();
+        if(!(delegate instanceof WildCardSSLVerifier)) {
+            sslSocketFactory.setHostnameVerifier(new WildCardSSLVerifier(delegate));
+        }
+        return client;
+    }
 
     public static void get(String url, RequestParams params, String token, Context context, AsyncHttpResponseHandler responseHandler) {
         if (isRequestReady(context, token)) client.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
     public static void getFeed(String url, RequestParams params, Context context, AsyncHttpResponseHandler responseHandler) {
+        if (client==null) client = getTolerantClient();
         if (UmbrellaUtil.isNetworkAvailable(context)) client.get(url, params, responseHandler);
     }
 
