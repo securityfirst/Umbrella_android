e7fdaac525713445d9cc4efcf209b314a9fc7d73
Rok Biderman <rok.biderman@gmail.com>
2015-07-04 15:41:58 -0700

    fix for about button

---
+++ app/src/main/java/org/secfirst/umbrella/AboutActivity.java
@@ -0,0 +1,54 @@
+package org.secfirst.umbrella;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+public class AboutActivity extends BaseActivity {
+
+    private WebView content;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getSupportActionBar()!=null) {
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        content = (WebView) findViewById(R.id.about_content);
+
+        final String html = "about html here";
+        content.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                content.loadDataWithBaseURL("file:///android_res/drawable/", "<style>body{color:#444444}img{width:100%}h1{color:#33b5e5; font-weight:normal;}h2{color:#9ABE2E; font-weight:normal;}a{color:#33b5e5}.button,.button:link{display:block;text-decoration:none;color:white;border:none;width:100%;text-align:center;border-radius:3px;padding-top:10px;padding-bottom:10px;}.green{background:#9ABE2E}.purple{background:#b83656}.yellow{background:#f3bc2b}</style>" + html, "text/html", "UTF-8", "UTF-8");
+            }
+        }, 100);
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_about;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_about, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id==android.R.id.home) {
+            startActivity(new Intent(this, MainActivity.class));
+        } else if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
