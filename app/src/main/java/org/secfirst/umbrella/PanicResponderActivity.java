ab8eb307610f39d1760cb152cf62d90301bccac7
Hans-Christoph Steiner <hans@eds.org>
2016-01-13 15:26:12 +0100

    respond to generic panic trigger Intent by logging out
    PanicKit provides a common framework for creating "panic button" apps that
    can trigger actions in "panic responder" apps.  In this case, the response
    is to log out in the app.

---
+++ app/src/main/java/org/secfirst/umbrella/PanicResponderActivity.java
@@ -0,0 +1,32 @@
+package org.secfirst.umbrella;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+import org.secfirst.umbrella.util.Global;
+
+public class PanicResponderActivity extends Activity {
+
+    public static final String PANIC_TRIGGER_ACTION = "info.guardianproject.panic.action.TRIGGER";
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        if (intent != null && PANIC_TRIGGER_ACTION.equals(intent.getAction())) {
+            Global global = (Global) getApplicationContext();
+            global.logout(this);
+        }
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            finishAndRemoveTask();
+        } else {
+            finish();
+        }
+    }
+}
