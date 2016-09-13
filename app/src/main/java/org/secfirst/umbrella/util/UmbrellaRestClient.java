06ace4c9b09eaaea5458aca68eab1b1f14ef217e
Rok Biderman <rok.biderman@gmail.com>
2016-09-13 21:11:47 +0200

    Added new cert pin

--- app/src/main/java/org/secfirst/umbrella/util/UmbrellaRestClient.java
+++ app/src/main/java/org/secfirst/umbrella/util/UmbrellaRestClient.java
@@ -26,7 +26,7 @@ public class UmbrellaRestClient {
 
     public static AsyncHttpClient getClientForApiUpdates(Context context) {
         AsyncHttpClient client = new AsyncHttpClient();
-        String[] pins                 = new String[] {"19ed92909228c6ffc29da6b79d05bc83bab15a78", "852627ad032bf9ab22e416cbbf9e32bc1187366f"};
+        String[] pins = new String[] {"19ed92909228c6ffc29da6b79d05bc83bab15a78", "852627ad032bf9ab22e416cbbf9e32bc1187366f", "1d2eb31886d384cf34986f57eb75a21d08f7aaef"};
         try {
             client.setSSLSocketFactory(new PinningSSLSocketFactory(context ,pins, 0));
         } catch (UnrecoverableKeyException | KeyManagementException | KeyStoreException | NoSuchAlgorithmException e) {
