1e641b06e129395e2e5f7b2f483e4dd06d14afc9
Rok Biderman <rok.biderman@gmail.com>
2017-12-20 14:19:59 +0100

    Remove unneeded exclusion strategy

--- app/src/main/java/org/secfirst/umbrella/MainActivity.java
+++ app/src/main/java/org/secfirst/umbrella/MainActivity.java
@@ -26,8 +26,6 @@ import android.widget.Toast;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
 import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.stmt.Where;
 
@@ -588,30 +586,6 @@ public class MainActivity extends BaseActivity implements DifficultyFragment.OnD
     @Override
     public void onDrawerStateChanged(int newState) {
         actionBarDrawerToggle.onDrawerStateChanged(newState);
-    }
-
-    private static class ExclusionForForms implements ExclusionStrategy {
-
-        public boolean shouldSkipClass(Class<?> arg0) {
-            return false;
-        }
-
-        public boolean shouldSkipField(FieldAttributes f) {
-            Timber.d("class: %s", f.getDeclaringClass().getName());
-            Timber.d("name: %s", f.getName());
-            switch (f.getDeclaringClass().getName()) {
-                case "org.secfirst.umbrella.models.Form":
-                    return !(f.getName().equals("title") || f.getName().equals("screens"));
-                case "org.secfirst.umbrella.models.FormScreen":
-                    return !(f.getName().equals("title") || f.getName().equals("items1"));
-//                case "org.secfirst.umbrella.models.FormItem":
-//                    return !(f.getName().equals("title") || f.getName().equals("type") || f.getName().equals("hint") || f.getName().equals("options"));
-//                case "org.secfirst.umbrella.models.FormOption":
-//                    return !(f.getName().equals("option"));
-            }
-            return true;
-        }
-
     }
 
 }
