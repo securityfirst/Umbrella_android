e997e994956cef961b1b51a502ef9a8251f2d3dd
Douglas Mesquita <douglasalipio@gmail.com>
2018-03-05 17:01:02 +0000

    Swipe refresh on webview

--- app/src/main/java/org/secfirst/umbrella/WebViewActivity.java
+++ app/src/main/java/org/secfirst/umbrella/WebViewActivity.java
@@ -7,6 +7,8 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -25,25 +27,55 @@ public class WebViewActivity extends BaseActivity {
 
     public static String URL_KEY;
     private ProgressBar mProgress;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private WebView mWebView;
+    private boolean mRefreshEnable;
+    private final static int MAX_TIME = 30000;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         UmbrellaUtil.setStatusBarColor(this, getResources().getColor(R.color.umbrella_purple_dark));
-        WebView webView = findViewById(R.id.article_web_view);
+        mWebView = findViewById(R.id.article_web_view);
         mProgress = findViewById(R.id.web_view_load);
+        mSwipeRefreshLayout = findViewById(R.id.web_view_swipe_refresh);
         enableActionBar();
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.clearHistory();
-        webView.clearFormData();
-        webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
-        webView.clearCache(true);
-        webView.setWebViewClient(new WebViewClient() {
+        setUpWebView();
+        setUpSwipeRefresh();
+        mWebView.loadUrl(getLink());
+    }
+
+    private void setUpSwipeRefresh() {
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                doRefresh();
+            }
+        });
+    }
+
+    private void doRefresh() {
+        mSwipeRefreshLayout.setRefreshing(true);
+        mProgress.setVisibility(View.INVISIBLE);
+        mRefreshEnable = true;
+        mWebView.loadUrl(getLink());
+    }
+
+
+    private void setUpWebView() {
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.clearHistory();
+        mWebView.clearFormData();
+        mWebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        mWebView.clearCache(true);
+        mWebView.setWebViewClient(new WebViewClient() {
             @SuppressWarnings("deprecation")
             @Override
             public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
                 Toast.makeText(WebViewActivity.this, description, Toast.LENGTH_SHORT).show();
+                mProgress.setVisibility(View.INVISIBLE);
+                mSwipeRefreshLayout.setEnabled(false);
             }
 
             @TargetApi(android.os.Build.VERSION_CODES.M)
@@ -51,29 +83,38 @@ public class WebViewActivity extends BaseActivity {
             public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
                 // Redirect to deprecated method, so you can use it in all SDK versions
                 onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+                mProgress.setVisibility(View.INVISIBLE);
+                mSwipeRefreshLayout.setEnabled(false);
+                if (mRefreshEnable) mSwipeRefreshLayout.setRefreshing(false);
             }
 
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                mProgress.setVisibility(View.VISIBLE);
+                if (!mRefreshEnable) mProgress.setVisibility(View.VISIBLE);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mProgress.setVisibility(View.INVISIBLE);
+                        mSwipeRefreshLayout.setRefreshing(false);
+                    }
+                }, MAX_TIME);
             }
 
             @Override
             public void onPageFinished(WebView view, String url) {
                 mProgress.setVisibility(View.INVISIBLE);
+                if (mRefreshEnable) mSwipeRefreshLayout.setRefreshing(false);
+
             }
         });
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             // chromium, enable hardware acceleration
-            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+            mWebView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
         } else {
             // older android version, disable hardware acceleration
-            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+            mWebView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
         }
-
-        webView.loadUrl(getLink());
-
     }
 
     @Override
@@ -90,8 +131,11 @@ public class WebViewActivity extends BaseActivity {
             case android.R.id.home:
                 finish();
                 break;
+            case R.id.web_view_menu_refresh:
+                doRefresh();
+                break;
 
-            case R.id.article_action_browser:
+            case R.id.web_view_action_browser:
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse(getLink()));
                 startActivity(intent);
