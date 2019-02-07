bf7819729dad49bc6aaee28fdb2210b4339656bc
Rok Biderman <rok.biderman@gmail.com>
2019-02-07 04:17:16 -0800

    Migrate to Androidx

--- app/src/main/java/org/secfirst/umbrella/whitelabel/feature/checklist/view/controller/ChecklistController.kt
+++ app/src/main/java/org/secfirst/umbrella/whitelabel/feature/checklist/view/controller/ChecklistController.kt
@@ -10,6 +10,9 @@ import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.bluelinelabs.conductor.RouterTransaction
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.alert_control.view.*
 import kotlinx.android.synthetic.main.checklist_add_item_dialog.view.*
 import kotlinx.android.synthetic.main.checklist_view.*
