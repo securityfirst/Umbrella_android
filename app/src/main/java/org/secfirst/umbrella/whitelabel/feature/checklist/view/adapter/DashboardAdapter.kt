9b66ce310def4c47f7534de30e08e3acd73f6379
Douglas Mesquita <douglasalipio@gmail.com>
2019-02-18 15:15:57 +0000

    Set a total_done icon.

--- app/src/main/java/org/secfirst/umbrella/whitelabel/feature/checklist/view/adapter/DashboardAdapter.kt
+++ app/src/main/java/org/secfirst/umbrella/whitelabel/feature/checklist/view/adapter/DashboardAdapter.kt
@@ -74,9 +74,13 @@ class DashboardAdapter(private val dashboardItems: MutableList<Dashboard.Item>,
             with(dashboardItem) {
                 itemView.itemLabel.text = label
                 itemView.itemPercentage.text = "$progress%"
-                setDifficultyColor(dashboardItem.levelLabel)
-                if (adapterPosition != 1)
+                val isCustomChecklist = dashboardItem.checklist?.custom ?: false
+                setDifficultyColor(dashboardItem.levelLabel, isCustomChecklist)
+                if (adapterPosition > 1)
                     itemView.setOnClickListener { clickListener(this@DashboardHolder) }
+                else
+                    itemView.levelColor
+                            .backgroundDrawable = ContextCompat.getDrawable(appContext(), R.drawable.ic_total_done)
 
                 itemView.setOnLongClickListener {
                     longClickListener(this@DashboardHolder)
@@ -85,15 +89,18 @@ class DashboardAdapter(private val dashboardItems: MutableList<Dashboard.Item>,
             }
         }
 
-        private fun setDifficultyColor(level: Int) {
+        private fun setDifficultyColor(level: Int, isCustomChecklist: Boolean) {
             when (level) {
                 Difficulty.BEGINNER -> itemView.levelColor.backgroundDrawable =
                         ContextCompat.getDrawable(appContext(), R.drawable.ic_beginner)
+
                 Difficulty.ADVANCED -> itemView.levelColor
                         .backgroundDrawable = ContextCompat.getDrawable(appContext(), R.drawable.ic_intermediate)
+
                 Difficulty.EXPERT -> itemView.levelColor
                         .backgroundDrawable = ContextCompat.getDrawable(appContext(), R.drawable.ic_expert)
-                else -> itemView.levelColor.visibility = View.GONE
+
+                else -> if (isCustomChecklist) itemView.levelColor.visibility = View.GONE
             }
         }
     }
