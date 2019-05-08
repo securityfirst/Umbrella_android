3ef720cede560474bfa1ffb4572b8557216f34cf
Jacopo <jacopo.carboni88@gmail.com>
2019-05-08 11:38:03 +0200

    glossary ordered alphabetically

--- app/src/main/java/org/secfirst/umbrella/feature/segment/SegmentPagination.kt
+++ app/src/main/java/org/secfirst/umbrella/feature/segment/SegmentPagination.kt
@@ -2,13 +2,20 @@ package org.secfirst.umbrella.feature.segment
 
 import org.secfirst.umbrella.data.database.segment.Markdown
 import org.secfirst.umbrella.data.database.segment.sortByIndex
+import org.secfirst.umbrella.data.disk.Template
 
 class SegmentPagination(private val markdowns: MutableList<Markdown>) {
 
     private val limitPage = 6
 
     fun nextPage(): List<Markdown> {
+        var glossary = false
         val pages = mutableListOf<Markdown>()
+        markdowns.forEach {
+            if (it.id.contains(Template.GLOSSARY.value)) {
+                glossary = true
+            }
+        }
 
         if (markdowns.size <= limitPage) {
             pages.addAll(markdowns)
@@ -19,6 +26,10 @@ class SegmentPagination(private val markdowns: MutableList<Markdown>) {
                 (0..limitPage).forEach { _ -> markdowns .removeAt(0) }
             }
         }
+        if (glossary){
+            return pages
+        }
+
         return pages.sortByIndex()
     }
 }
